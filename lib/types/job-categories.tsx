7663a986af59cf76a90ea51ce749201c6c91b0d6
Akhil Tilak <akhil.tilak@webofficeit.com>
2025-11-05 15:56:28 +0530

    categories queries for filter categories

--- lib/types/job-categories.tsx
+++ lib/types/job-categories.tsx
@@ -3,3 +3,19 @@ export type JobCategories = {
     name?: String;
     slug?: String;
 }
+
+export type FilteredJobCategories = {
+    items: JobCategories[]
+    page: number
+    page_size: number
+    total_items: number
+    total_pages: number
+}
+
+export type FilteredJobCategoriesResponse = { success: boolean; message: string, data: FilteredJobCategories };
+
+export type FilterJobCategoriesData = {
+    q?: string;   
+    page?: number;
+    page_size?: number;
+};
