50c17f777efcb99bb6f34d08698e6324b64bd426
Akhil Tilak <akhil.tilak@webofficeit.com>
2025-11-25 09:22:45 +0530

    dragable scroll issue and modal open issue

--- components/ManageJobs/JobTable.tsx
+++ components/ManageJobs/JobTable.tsx
@@ -35,7 +35,7 @@ const JobTable = ({ jobs, t, setJobs, page, pageSize }) => {
                         <th
                             className="p-4 transition-colors cursor-pointer border-y border-slate-200 bg-slate-50 hover:bg-slate-100">
                             <p
-                                className="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none text-slate-500 pl-11">
+                                className="flex items-center justify-between gap-2 font-sans text-sm font-normal leading-none text-slate-500">
                                 {t("list.table.title")}
                             </p>
                         </th>
